c33cbc9ce2283df3f2d60214ba1e4f95ac68442a
Arthur NKhoma <arthurnkhoma78@gmail.com>
2026-01-30 08:10:39 +0200

    fixed the api.js fetching problem

--- src/services/api.js
+++ src/services/api.js
@@ -2,13 +2,13 @@ const API_URL = import.meta.env.VITE_API_URL;
 
 
 export const fetchCars = async () => {
-  const res = await fetch(`${API_URL}/cars`);
+  const res = await fetch(`${API_URL}cars`);
   if (!res.ok) throw new Error("Failed to fetch cars");
   return res.json();
 };
 
 export const fetchCarById = async (id) => {
-  const res = await fetch(`${API_URL}/cars`);
+  const res = await fetch(`${API_URL}cars`);
   const cars = await res.json();
   return cars.find((car) => car.id === Number(id));
 };
